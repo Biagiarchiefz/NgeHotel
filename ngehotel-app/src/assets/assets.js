e9e1c16c986667d75046614941ea0cd481040afc
Biagiarchiefz <biagiiarchie@gmail.com>
2025-07-25 21:17:58 +0700

    feat : add section Exlusive offers

--- ngehotel-app/src/assets/assets.js
+++ ngehotel-app/src/assets/assets.js
@@ -81,7 +81,7 @@ export const cities = [
     "London",
 ];
 
-// Exclusive Offers Dummy Data
+// Exclusive Offers Dummy Data  
 export const exclusiveOffers = [
     { _id: 1, title: "Summer Escape Package", description: "Enjoy a complimentary night and daily breakfast", priceOff: 25, expiryDate: "Aug 31", image: exclusiveOfferCardImg1 },
     { _id: 2, title: "Romantic Getaway", description: "Special couples package including spa treatment", priceOff: 20, expiryDate: "Sep 20", image: exclusiveOfferCardImg2 },
--- ngehotel-app/src/components/FeaturedDestination.jsx
+++ ngehotel-app/src/components/FeaturedDestination.jsx
@@ -21,8 +21,8 @@ const FeaturedDestination = () => {
         ))}
       </div>
 
-        {/* ptyn kenapa dionClick menggunakan ; di navigate */}
-      <button onClick={() => {navigate("/rooms"); scrollTo(0, 0) } } className="my-16 px-4 py-2 text-sm font-medium border border-gray-300 rounded bg-white hover:bg-gray-50 transition-all cursor-pointer">
+
+      <button onClick={() => { navigate("/rooms"); scrollTo(0, 0) } } className="my-16 px-4 py-2 text-sm font-medium border border-gray-300 rounded bg-white hover:bg-gray-50 transition-all cursor-pointer">
         View All Destinations
       </button>
     </div>
