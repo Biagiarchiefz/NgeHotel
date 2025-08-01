0a1eb8639754918ce643f312c32044427afd8a47
Biagiarchiefz <biagiiarchie@gmail.com>
2025-08-01 20:17:02 +0700

    feat : add layout sidebar for owner

--- ngehotel-app/src/App.jsx
+++ ngehotel-app/src/App.jsx
@@ -6,6 +6,8 @@ import Footer from "./components/Footer";
 import AllRooms from "./pages/AllRooms";
 import RoomDetails from "./pages/RoomDetails";
 import MyBookings from "./pages/MyBookings";
+import HotelReg from "./components/HotelReg";
+import Layout from "./pages/hotelOwner/Layout";
 
 const App = () => {
   // useLocation fungsi dari React Router yang digunakan untuk mengambil informasi URL yang sedang dibuka di browser.
@@ -16,12 +18,18 @@ const App = () => {
     <div>
       {/* jika path bukan owner, tampilkan navbar */}
       {!isOwnerPath && <Navbar />}
+      {false && <HotelReg/>}
+
+      {/* kenapa di bungkus dengan div h-70vh??..., karena untuk Membantu penataan layout Khususnya dalam layout header → konten → footer, kita sering pakai min-h supaya kontennya "mengisi ruang" dan footer tidak naik ke tengah jika isi konten pendek. */}
       <div className="min-h-[70vh]">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/rooms" element={<AllRooms />} />
           <Route path="/rooms/:id" element={<RoomDetails />} />
           <Route path="/my-bookings" element={<MyBookings />} />
+          <Route path="/owner" element={<Layout />}>
+            
+          </Route>
         </Routes>
       </div>
 
